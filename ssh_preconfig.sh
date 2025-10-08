22a5c2f2f588ff6e44538604df620d046cbaf9c5
LilBro <alejandroalo33@proton.me>
2025-10-08 17:18:42 +0200

    Update ssh_preconfig.sh

--- ssh_preconfig.sh
+++ ssh_preconfig.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # This script is the pre-configuration before performing the simulated attack to modify the Raspberry Pi GPIO
 
@@ -6,14 +6,14 @@
 # curl -sL https://raw.githubusercontent.com/L1LBRO/Modificar-GPIO-Raspberry-Pi/refs/heads/main/Configuracion_previa_ssh.sh | sudo bash -s
 
 # Define color constants
-cColorAzul='\033[0;34m'
-cColorAzulClaro='\033[1;34m'
-cColorVerde='\033[1;32m'
-cColorRojo='\033[1;31m'
-cFinColor='\033[0m'
+colorBlue='\033[0;34m'
+colorLightBlue='\033[1;34m'
+colorGreen='\033[1;32m'
+colorRed='\033[1;31m'
+colorEnd='\033[0m'
 
 echo ""
-echo -e "${cColorAzulClaro} Starting the Raspberry Pi GPIO modification script...${cFinColor}"
+echo -e "${colorLightBlue} Starting the Raspberry Pi GPIO modification script...${colorEnd}"
 echo ""
 
 # Verify number of parameters
@@ -25,11 +25,11 @@ if [ $# -ne 5 ]; then
 fi
 
 # Parameters
-vIpRaspberry=$1
-vPassp=$2
-vUser=$3
-vGpio=$4
-vRaspPass=$5 
+RPI_IP=$1
+SSH_PASSPHRASE=$2
+RPI_USER=$3
+PLC_INPUT=$4
+RPI_PASSWORD=$5 
 
 # Generate ssh keys
 echo ""
@@ -37,19 +37,19 @@ echo "Generating SSH keys to connect to the Raspberry Pi..."
 echo ""
 
 # Key file path
-vKey_Name="${HOME}/.ssh/id_rsa"
+SSH_KEY_PATH="${HOME}/.ssh/id_rsa"
 
 # Remove if the key already exists
-if [ -f "$vKey_Name" ]; then
-    rm -f "$vKey_Name"
+if [ -f "$SSH_KEY_PATH" ]; then
+    rm -f "$SSH_KEY_PATH"
 fi
 
 # Generate the ssh key
-ssh-keygen -t rsa -b 4096 -f "$vKey_Name" -C "$vUser@$vIpRaspberry" -N "$vPassp"
+ssh-keygen -t rsa -b 4096 -f "$SSH_KEY_PATH" -C "$RPI_USER@$RPI_IP" -N "$SSH_PASSPHRASE"
 
 if [ $? -eq 0 ]; then
     echo ""
-    echo "SSH key generated successfully at $vKey_Name."
+    echo "SSH key generated successfully at $SSH_KEY_PATH."
     echo ""
 else
     echo ""
@@ -71,7 +71,7 @@ chmod 644 "${HOME}/.ssh/id_rsa.pub"
 
 # Use sshpass to copy the SSH key with the provided password
 echo "Sending the SSH public key with ssh-copy-id..."
-sshpass -p "$vRaspPass" ssh-copy-id -o StrictHostKeyChecking=no -i "${HOME}/.ssh/id_rsa.pub" "$vUser@$vIpRaspberry"
+sshpass -p "$RPI_PASSWORD" ssh-copy-id -o StrictHostKeyChecking=no -i "${HOME}/.ssh/id_rsa.pub" "$RPI_USER@$RPI_IP"
 
 if [ $? -eq 0 ]; then
     echo ""
@@ -130,4 +130,4 @@ fi
 
 # Call the Python script to modify the GPIO
 echo "Running Python script to modify GPIO..."
-curl -sL https://raw.githubusercontent.com/L1LBRO/Modify-GPIO-Raspberry-Pi/refs/heads/main/Gpio_Mod_exit.py | python3 - "$vIpRaspberry" "$vKey_Name" "$vPassp" "$vUser" "$vGpio"
+curl -sL https://raw.githubusercontent.com/L1LBRO/Modify-GPIO-Raspberry-Pi/refs/heads/main/Gpio_Mod_exit.py | python3 - "$RPI_IP" "$SSH_KEY_PATH" "$SSH_PASSPHRASE" "$RPI_USER" "$PLC_INPUT"
